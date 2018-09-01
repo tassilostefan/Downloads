7e6722f93b8dd2d6d4ce10018c2e715f58d5c352
tassilostefan <geis.stefan+git@gmail.com>
2018-09-01 15:41:23 +0200

    Save only selected columns
    Save only selected columns of the final depot csv file
    Prepare function import2Xls to import csv to existing excel

--- Import-Dividends.ps1
+++ Import-Dividends.ps1
@@ -24,6 +24,7 @@ $reader = New-Object iTextSharp.text.pdf.pdfreader -ArgumentList $pdfPath
 }
 
 #removes header lines in csv from $csvPath file as long as the no. of clumns is less than $minNoOfCsvColumns 
+#removes double currency entries ( columns 
 function cleanCsvHeader($csvPath){
     $skip = 0
     (Get-Content $csvPath).Replace("Gesamtperformance Currency;%", "Gesamtperformance Currency;Gesamtperformance_%").Replace("Tagesperformance Currency;%","Tagesperformance Currency;Tagesperformance_%") | Set-Content $csvPath
@@ -33,6 +34,56 @@ function cleanCsvHeader($csvPath){
     if($skip -gt 0){
         (Get-Content $csvPath | Select-Object -Skip $skip) | Set-Content $csvPath
     }
+    $filteredCsv = Import-Csv $csvPath -Delimiter ";" | Select-Object Bestand, Name, ISIN, WKN, Typ, Datum, Zeit, 'Akt. Geldkurs', 'Akt. Geldkurs Currency', Boerse, Kaufkurs, Kaufwert, Gesamtperformance, Gesamtperformance_%, Tagesperformance, Tagesperformance_%, 'Aktueller Wert', Wert 
+    $filteredCsv | Export-Csv $csvPath -Delimiter ";" -NoTypeInformation
+}
+
+function import2Xls($csvFile){
+    Add-Type -AssemblyName Microsoft.Office.Interop.Excel
+
+    $Excel = New-Object -ComObject Excel.Application
+    $xlLastCell = [Microsoft.Office.Interop.Excel.Constants]::xlLastCell
+    $xlFile = $csvFile.Replace(".csv", ".xlsx")
+
+    $Excel.visible = $false 
+    $Excel.displayalerts=$False 
+
+    $ExcelWordBook = $Excel.Workbooks.Open($xlFile)
+    $ExcelWorkSheet = $Excel.WorkSheets.item('Depotübersicht_Dividenden')
+    $ExcelWorkSheet.activate()
+
+    $objRange = $ExcelWorkSheet.UsedRange
+    $lastRow = 0
+#    $lastRow = $objRange.SpecialCells($xlLastCell).Row
+#    $ExcelWorkSheet.cells($lastRow,1).Select()
+
+    $singers = Import-Csv -Path $csvFile
+    $singers.foreach{
+      $lastRow += 1
+      $ExcelWorkSheet.cells($lastRow,1).value = $_.Name
+      $ExcelWorkSheet.cells($lastRow,2).value = $_.ISIN
+      $ExcelWorkSheet.cells($lastRow,3).value = $_.WKN
+      $ExcelWorkSheet.cells($lastRow,4).value = $_.Typ
+      $ExcelWorkSheet.cells($lastRow,5).value = $_.Datum
+      $ExcelWorkSheet.cells($lastRow,6).value = $_.Zeit
+      $ExcelWorkSheet.cells($lastRow,7).value = $_.Bestand
+      $ExcelWorkSheet.cells($lastRow,8).value = $_.'Akt. Geldkurs'
+      $ExcelWorkSheet.cells($lastRow,9).value = $_.'Akt. Geldkurs Currency'
+      $ExcelWorkSheet.cells($lastRow,10).value = $_.Tagesperformance
+      $ExcelWorkSheet.cells($lastRow,11).value = $_.'Tagesperformance_%'
+      $ExcelWorkSheet.cells($lastRow,12).value = $_.'Aktueller Wert'
+      $ExcelWorkSheet.cells($lastRow,13).value = $_.Wert
+      $ExcelWorkSheet.cells($lastRow,14).value = $_.Kaufdatum
+      $ExcelWorkSheet.cells($lastRow,15).value = $_.Kaufkurs
+      $ExcelWorkSheet.cells($lastRow,16).value = $_.Kaufwert
+      $ExcelWorkSheet.cells($lastRow,17).value = $_.Dividende
+    }
+
+    $ExcelWordBook.Save()
+    $ExcelWordBook.Close()
+
+    $Excel.Quit()
+    stop-process -processname EXCEL   
 }
 
 Get-ChildItem ($Directory + "*Wertpapierertrag.pdf") -File |  %{
@@ -45,7 +96,6 @@ Get-ChildItem ($Directory + "*Wertpapierertrag.pdf") -File |  %{
     ISIN = 0
     Anteile = 0
     DivPStueck = 0
-    DivPStueckWhrg = ''
     Zeitraum = ''
     Datum = ''
     Start = ''
@@ -61,7 +111,6 @@ Get-ChildItem ($Directory + "*Wertpapierertrag.pdf") -File |  %{
             $obj.ISIN =  $targetLines[$lineIdx + 3]
             $obj.Anteile =  $targetLines[$lineIdx + 5] -replace "STK "
             $obj.DivPStueck =  $targetLines[$lineIdx + 11].ToString().Split(" ")[1]
-            $obj.DivPStueckWhrg =  $targetLines[$lineIdx + 11].ToString().Split(" ")[0]
             if($targetLines[$lineIdx + 13].ToString().Length -lt 24) {
                 $obj.Zeitraum =  $targetLines[$lineIdx + 13].ToString()
                 $obj.Start =  $targetLines[$lineIdx + 13].ToString().Split(" - ")[0]
@@ -98,7 +147,6 @@ Get-ChildItem ($Directory + "*Wertpapierabrechnung.pdf") -File |  %{
     Anteile = 0
     Kurswert = 0
     Datum = ''
-    Waehrg = ''
     }
 
     for($lineIdx = 0; $lineIdx -le $targetLines.Count;$lineIdx++){
@@ -108,7 +156,6 @@ Get-ChildItem ($Directory + "*Wertpapierabrechnung.pdf") -File |  %{
             $obj.ISIN =  $targetLines[$lineIdx + 3]
             $obj.Anteile =  $targetLines[$lineIdx + 5] -replace "STK "
             $obj.Kurswert =  $targetLines[$lineIdx + 13]
-            $obj.Waehrg =  $targetLines[$lineIdx + 12]
         } elseif ($targetLines[$lineIdx] -eq "Handelstag"){
             $obj.Datum =  $targetLines[$lineIdx + 1]
             if($obj.ISIN -eq "US7565771026"){$obj.Datum = "01.10.1995"}
@@ -122,35 +169,30 @@ Get-ChildItem ($Directory + "*Wertpapierabrechnung.pdf") -File |  %{
     Anteile = 0
     Kurswert = 0
     Datum = ''
-    Waehrg = ''
     }
         }
 
     }
 
-    $obj = [PSCustomObject]@{
+}
+$obj = [PSCustomObject]@{
     Datei = "code-generated"
     Aktie = 'Red Hat'
     ISIN = "US7565771026"
     Anteile = 60
     Kurswert = 25
     Datum = '01.10.1995'
-    Waehrg = 'EUR'
     }
-    $KaufObjs.Add($obj)
-    $obj = [PSCustomObject]@{
+$KaufObjs.Add($obj)
+$obj = [PSCustomObject]@{
     Datei = "code-generated"
     Aktie = 'SAG Solrastrom'
     ISIN = "702100"
     Anteile = 1
     Kurswert = 25
     Datum = '01.10.2013'
-    Waehrg = 'EUR'
     }
-    $KaufObjs.Add($obj)
-
-
-}
+$KaufObjs.Add($obj)
 
 $KaufObjs | Export-Csv -Path ($Directory + "kaufdaten.csv") -NoTypeInformation -Delimiter ";"
 
@@ -164,5 +206,6 @@ $depot = Import-Csv -Path $depotPath -Delimiter ";"
 $depot = Import-Csv -Path $depotPath -Delimiter ";" | Select-Object *,@{Name='Kaufdatum';Expression={$isin = $_.ISIN
 ($KaufObjs | where ISIN -EQ $isin | Select-Object Datum)[0].Datum}},@{Name='Dividende';Expression={$isin = $_.ISIN
 ($DividendenObj | where ISIN -EQ $isin | Select-Object  @{Name="Surname";Expression={[convert]::ToDouble($_.Dividende)}} | Measure-Object -Property "surname" -Sum).Sum}} 
-
-$depot | Export-Csv -Path ($Directory + "Depotübersicht_Dividenden.csv") -NoTypeInformation -Delimiter ";"
+$csvPath = ($Directory + "Depotübersicht_Dividenden.csv")
+$depot | Select-Object -First ($depot.Length-2) | Select-Object Name, ISIN, WKN, Typ, Datum, Zeit, Bestand, 'Akt. Geldkurs', 'Akt. Geldkurs Currency', Boerse, Tagesperformance, Tagesperformance_%, 'Aktueller Wert', Wert, Kaufdatum, Kaufkurs, Kaufwert, Dividende | Export-Csv -Path $csvPath -NoTypeInformation -Delimiter ";"
+#importXls($csvPath)
